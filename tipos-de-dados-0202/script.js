a014c64d0921ae7b157c1824754f76e7fcce70b4
FelipeKttu <kttudev@gmail.com>
2024-06-11 02:59:32 -0300

    commit inicial

---
+++ tipos-de-dados-0202/script.js
@@ -0,0 +1,9 @@
+var nome = "felipe";
+var idade = 20;
+var time = "corinthians ";
+var simbolo = Symbol();
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof time);
+console.log(typeof simbolo);
