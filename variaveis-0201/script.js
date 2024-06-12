7c5846bef6e1e247a9e568994243f8285cf5c71d
FelipeKttu <kttudev@gmail.com>
2024-06-12 03:40:12 -0300

    add_comentarios

---
+++ variaveis-0201/script.js
@@ -0,0 +1,18 @@
+// Declarar uma variável com o seu nome
+var nome = "Felipe Vieira da Silva";
+
+// Declarar uma variável com a sua idade
+idade = "20";
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = "Pão de queijo";
+
+// Declarar 5 variáveis diferentes sem valores
+var lerolero, biroliro, labadacio, devereli, tchubi;
+
+console.log(comidaFavorita, nome, idade);
